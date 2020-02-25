b11db4f68838208dda2faab15a149cf699a39b4e
JiaruFu <43692362+JiaruFu@users.noreply.github.com>
2020-02-25 15:43:44 -0600

    Add files via upload

--- condor_log.py
+++ condor_log.py
@@ -34,7 +34,6 @@ def parse_args():
 
     # select which jobs to track
 
-
     parser.add_argument(
         "-files", nargs="+", metavar="FILE", help="Which event logs to track."
     )
@@ -59,75 +58,83 @@ def cli():
     )
 
 def condor_log(event_logs = None):
-    if event_logs is not None:
+ if event_logs is not None:
          for f in event_logs:
                 jel = htcondor.JobEventLog(f)
                 job_time_slot = {}
                 job_event = []
                 for event in jel.events(0):
-                    job_status = event.type
-                    if str(job_status) not in job_event and str(job_status) != "JOB_TERMINATED":
-                            job_event.append(str(job_status))
+                   job_status = event.type
+                   if str(job_status) not in job_event:
+                         job_event.append(str(job_status))
 
-                    cluster_id =event.cluster
-                    if cluster_id not in job_time_slot:
+                   cluster_id =event.cluster
+                   if cluster_id not in job_time_slot:
                           job_time_slot[cluster_id]={}
 
-                    job_id = event.proc
-                    if job_id not in job_time_slot[cluster_id].keys():
+                   job_id = event.proc
+                   if job_id not in job_time_slot[cluster_id].keys():
                           job_time_slot[cluster_id][job_id]={}
 
-                    timestamp = event.timestamp
-                    dt_object = datetime.fromtimestamp(timestamp)
-                    job_time_slot[cluster_id][job_id][str(job_status)] = dt_object
+                   timestamp = event.timestamp
+                   dt_object = datetime.fromtimestamp(timestamp)
+                   job_time_slot[cluster_id][job_id][str(job_status)] = dt_object.strftime("%Y-%m-%d %H:%M:%S")
 
-                job_time = duration(job_time_slot)
-                table(job_time, job_event)
-
+                table(job_time_slot,job_event )
                 #else:
                  #   print("We found the the end of file")
 
 
 
 def table(job_time_slot, job_event):
-    headers =["CLUSTER"]+["JOBS"]+job_event
-    processed_rows = []
-    print("".join("{:<15}".format(h) for h in headers))
-    output =""
-    for cluster in job_time_slot.keys():
-        for row in job_time_slot[cluster].items():
-             row = list(row)
-             line = [str(cluster)] + [str(row[0])] + [str(row[1].get(key)) for key in job_event]
-             processed_rows.append(line)     
-             print( "".join("{:<15}".format(m) for m in line))
+     headers =["CLUSTER"]+["JOBS"]+["RUN"] + ["IDLE"] + ["HOLD"]
+     status = ["RUN"] + ["IDLE"] + ["HOLD"]
+     print("".join("{:<15}".format(h) for h in headers))
+                ##event sorted with order according to dates
+      for cluster_id in job_time_slot:
+            for job_id in job_time_slot[cluster_id]:
+                    job_info = job_time_slot[cluster_id][job_id]
+                    job_time_slot[cluster_id][job_id] =  sorted(job_info.items(),key = lambda date: datetime.strptime(date[1],"%Y-%m-%d %H:%M:%S"))
+                    row = duration(job_time_slot[cluster_id][job_id], job_event)
+                    line = [str(cluster_id)] + [str(job_id)] + [str(row.get(key)) for key in status]
+                    print( "".join("{:<15}".format(m) for m in line))
 
 
-def duration(job_time_slot):
-   job_time = {}
-   for cluster_key, v in job_time_slot.items():
-        if cluster_key not in job_time.keys():
-                job_time[cluster_key] = {}
-        for job_key,time in v.items():
-              if job_key not in job_time[cluster_key].keys():
-                    job_time[cluster_key][job_key] = {}
-              temp_list = []
-              for t in time.keys():
-                   if t not in job_time[cluster_key][job_key].keys():
-                        job_time[cluster_key][job_key][t] = ""
-                   temp_list.append(job_time_slot[cluster_key][job_key][t].strftime("%Y-%m-%d %H:%M:%S"))
-              temp_list.sort(key = lambda date: datetime.strptime(date,"%Y-%m-%d %H:%M:%S"))
-              i = 0
-              for i in range(len(temp_list)):
-                   if(i+1<len(temp_list)):
-                        later_time = temp_list[i+1]
-                        early_time = temp_list[i]
-                        diff =  datetime.strptime(later_time, datetimeFormat)\
-                              - datetime.strptime(early_time, datetimeFormat)
-                        for t in time.keys():
-                             if str(job_time_slot[cluster_key][job_key][t]) == early_time:
-                                   job_time[cluster_key][job_key][t] = str(diff)
 
-   return job_time
+def duration(job_time, job_event):
+    event =  dict((e,False) for e in job_event)
+    next_event = {}
+    impo_event = ["SUBMIT", "EXECUTE", "JOB_TERMINATED", "JOB_RELEASED" ,"JOB_HELD"]
+    duration = {"IDLE": "0:0:0" , "HOLD": "0:0:0", "RUN": "0:0:0"}
+    for i in range(len(job_time)):
+       if (i+1)<len(job_time) and event[job_time[i][0]] == False:
+         if job_time[i][0] == "JOB_HELD":
+            j = i
+            while((job_time[j][0] not in impo_event or job_time[j][0] == "JOB_HELD")):
+                event[job_time[j][0]] = True
+                diff = datetime.strptime(job_time[i+1][1], datetimeFormat)\
+                             - datetime.strptime(job_time[i][1], datetimeFormat)
+                duration["HOLD"]=(datetime.strptime(duration["HOLD"],"%H:%M:%S")+ diff).strftime("%H:%M:%S")
+
+                j = j+1
+            continue
+
+         if job_time[i][0] == "EXECUTE":
+            j = i
+            while((job_time[j][0] not in impo_event or job_time[j][0] == "EXECUTE")):
+                event[job_time[j][0]] = True
+                diff =  datetime.strptime(job_time[j+1][1], datetimeFormat)\
+                           - datetime.strptime(job_time[j][1], datetimeFormat)
+                duration["RUN"]=(datetime.strptime(duration["RUN"],"%H:%M:%S")+ diff).strftime("%H:%M:%S")
+                j =j+1
+            continue
+
+         event[job_time[i][0]] = True
+         diff =  datetime.strptime(job_time[i+1][1], datetimeFormat)\
+                             - datetime.strptime(job_time[i][1], datetimeFormat)
+         duration["IDLE"]=(datetime.strptime(duration["IDLE"],"%H:%M:%S")+ diff).strftime("%H:%M:%S")
+
+    return duration
 
 
 if __name__ == "__main__":
