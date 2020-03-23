8ffe31584602db18e44e064cfd7a14875edbcc30
JiaruFu <43692362+JiaruFu@users.noreply.github.com>
2020-03-23 11:05:54 -0500

    Update graph.py

--- graph.py
+++ graph.py
@@ -50,27 +50,6 @@ def graph(time):
 
     p.xaxis.formatter = xformatter
                
-    #fruits = ['Apples', 'Pears', 'Nectarines', 'Plums', 'Grapes', 'Strawberries']
-    #years = ["2015", "2016", "2017"]
-
-    #exports = {'fruits' : fruits,
-        #       '2015'   : [2, 1, 4, 3, 2, 4],
-         #      '2016'   : [5, 3, 4, 2, 4, 6],
-          #     '2017'   : [3, 2, 4, 4, 5, 3]}
-  #  imports = {'fruits' : fruits,
-   #            '2015'   : [-1, 0, -1, -3, -2, -1],
-  #             '2016'   : [-2, -1, -3, -1, -2, -2],
-   #            '2017'   : [-1, -2, -1, 0, -2, -2]}
-
-    #p = figure(y_range=fruits, plot_height=250, x_range=(-16, 16), title="Fruit import/export, by year",
-    #           toolbar_location=None)
-
-   # p.hbar_stack(years, y='fruits', height=0.9, color=GnBu3, source=ColumnDataSource(exports),
-    #             legend_label=["%s exports" % x for x in years])
-
-    #p.hbar_stack(years, y='fruits', height=0.9, color=OrRd3, source=ColumnDataSource(imports),
-    #             legend_label=["%s imports" % x for x in years])
-
     p.y_range.range_padding = 0.1
     p.ygrid.grid_line_color = None
     p.legend.location = "top_left"
